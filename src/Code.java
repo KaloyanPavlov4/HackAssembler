62bf3a4c24c1e9e6e25c7cfc9d79af14e1726dcd
Kaloyan <kaloyanpavlov4@gmail.com>
2024-09-03 21:41:20 +0300

    Implement binary code generator

---
+++ src/Code.java
@@ -0,0 +1,42 @@
+
+import java.util.Map;
+
+public class Code {
+    private final Map<String, String> comp = Map.ofEntries(Map.entry("0", "0101010"), Map.entry("1", "0111111"), Map.entry("-1","0111010"),
+            Map.entry("D", "0001100"), Map.entry("A", "0110000"), Map.entry("!D", "0001101"), Map.entry("!A", "110001"),
+            Map.entry("-D", "0001111"), Map.entry("-A", "0110011"), Map.entry("D+1", "0011111"), Map.entry("A+1","0110111"),
+            Map.entry("D-1","0001110"),Map.entry("A-1","0110010"), Map.entry("D+A","0000010"), Map.entry("D-A","0010011"),
+            Map.entry("A-D","0000111"), Map.entry("D&A","0000000"), Map.entry("D|A","0010101"), Map.entry("M","1110000"),
+            Map.entry("!M","1110001"), Map.entry("-M","1110011"), Map.entry("M+1","1110111"), Map.entry("M-1","1110111"),
+            Map.entry("D+M","1000010"), Map.entry("D-M","1010011"), Map.entry("M-D","1000111"), Map.entry("D&M","1000000"),
+            Map.entry("D|M", "1010101"));
+    private final Map<String, String> dest = Map.ofEntries(Map.entry("M","001"), Map.entry("D","010"), Map.entry("MD","011"),
+            Map.entry("A","100"), Map.entry("AM","101"), Map.entry("AD","110"), Map.entry("AMD","111"));
+
+    private final Map<String, String> jump = Map.ofEntries(Map.entry("JGT","001"), Map.entry("JEQ","010"),Map.entry("JGE","001"),
+            Map.entry("JLT","100"),Map.entry("JNE","101"),Map.entry("JLE","110"),Map.entry("JMP","111"));
+
+    private String comp(String comp){
+        return this.comp.get(comp);
+    }
+
+    private String dest(String dest){
+        if(dest == null) return "000";
+        return this.dest.get(dest);
+    }
+
+    private String jump(String jump){
+        if(jump == null) return "000";
+        return this.jump.get(jump);
+    }
+
+    public String c_command(String comp, String dest, String jump){
+        return "111" + comp(comp) + dest(dest) + jump(jump);
+    }
+
+    public String a_command(int location){
+        String as16bitBinary = Integer.toBinaryString(0x10000 | location).substring(1);
+        return as16bitBinary;
+    }
+
+}
